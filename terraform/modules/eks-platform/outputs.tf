c0131efc982ec5947ac07889de61c7b7ed4901b6
Elzabeth-L <307373@ust.com>
2026-08-09 15:59:23 +0530

    Fix EKS add-on bootstrap ordering

--- terraform/modules/eks-platform/outputs.tf
+++ terraform/modules/eks-platform/outputs.tf
@@ -4,4 +4,9 @@ output "cluster_version" { value = aws_eks_cluster.this.version }
 output "ecr_repository_url" { value = aws_ecr_repository.notes.repository_url }
 output "database_secret_arn" { value = aws_secretsmanager_secret.database.arn }
 output "load_balancer_controller_role_arn" { value = aws_iam_role.load_balancer_controller.arn }
-output "addon_versions" { value = { for name, addon in aws_eks_addon.selected : name => addon.addon_version } }
+output "addon_versions" {
+  value = merge(
+    { for name, addon in aws_eks_addon.bootstrap : name => addon.addon_version },
+    { for name, addon in aws_eks_addon.post_node : name => addon.addon_version },
+  )
+}
