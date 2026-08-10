737465750065e98e2800416a6560ef447061ecc6
Elzabeth-L <307373@ust.com>
2026-08-10 09:01:10 +0530

    Allow container access to EC2 role credentials (#20)

--- terraform/modules/ec2/main.tf
+++ terraform/modules/ec2/main.tf
@@ -149,7 +149,7 @@ resource "aws_instance" "app" {
   metadata_options {
     http_endpoint               = "enabled"
     http_tokens                 = "required"
-    http_put_response_hop_limit = 1
+    http_put_response_hop_limit = 2
   }
 
   root_block_device {
